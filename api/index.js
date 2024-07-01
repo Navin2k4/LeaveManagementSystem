597233fdbc4cced7b9848d85892e6e9b6c06dea0
Navin Kumaran <navinkumaran2004@gmail.com>
2024-07-01 17:07:21 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+// import userRoutes from './routes/user.route.js';
+// import authRoutes from './routes/auth.route.js';
+// import postRoutes from './routes/post.route.js';
+// import commentRoutes from './routes/comment.route.js';
+
+// import path from 'path';
+
+dotenv.config();
+
+// Creating Database Connection
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("MongoDb is connected");
+    }).catch(err => {
+        console.log("MongoDb not connected");
+    });
+
+// const __dirname = path.resolve();
+
+// Creating the app
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Creating test API
+// app.use('/api/user', userRoutes);
+// app.use('/api/auth', authRoutes);
+// app.use('/api/post', postRoutes);
+// app.use('/api/comment', commentRoutes);
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
