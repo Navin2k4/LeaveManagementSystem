7f80ab24ea0279b3171aca3c6c6f0c9268a81242
Navin Kumaran <navinkumaran2004@gmail.com>
2024-07-07 11:16:50 +0530

    dep change

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,7 +14,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
-  },
 });
