4d951cf41f4d41edb760a9a63cddb16e5dfd8cfc
Navin Kumaran <navinkumaran2004@gmail.com>
2024-07-07 11:27:42 +0530

    ...

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use("/api", leaveRequestRoutes);
 app.use('/api', departmentRoutes);
 
 
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
